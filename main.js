b2fcf4a18366cb0b28113969c3384021b263f059
Toni Dew <Dewsy@Tonis-MacBook-Pro.local>
2016-01-28 11:17:19 -0600

    first commit of main.js file

---
+++ main.js
@@ -0,0 +1,15 @@
+/*
+textarea id="translator-input"
+option id="spanish"
+option id="german"
+option id="urdu"
+button id="translate-button"
+div id="translated-card"
+*/
+var inputText = document.getElementById("translator-input");
+var spanishInput = document.getElementById("spanish");
+var germanInput = document.getElementById("german");
+var urduInput = document.getElementById("urdu");
+var translateButton = document.getElementById("translate-button");
+var translatedCard = document.getElementById("translated-card");
+
